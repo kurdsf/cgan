9766e8b593c91431e01997195320a4bb43b3d992
kurdsf <user.kurtpieper@protonmail.com>
2023-03-29 23:55:55 +0200

    We test nn_forward and nn_backward by making sure a neural network can learn xor.

--- test.c
+++ test.c
@@ -1,7 +1,50 @@
 #include "nn.h"
 #include "utest.h"
 #include <gsl/gsl_matrix.h>
+#include <gsl/gsl_vector.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+#define NN_ITERATIONS 100
+#define NN_MAX_AVG_ERR 0.2
+
+static double vector_avg(const gsl_vector *x) {
+  return gsl_vector_sum(x) / ((double)x->size);
+}
+
+UTEST(nn, learn_xor) {
+  gsl_vector *input = gsl_vector_alloc(2);
+  gsl_vector *label = gsl_vector_alloc(1);
+  nn_t *nn = new_nn(2, 3, 1);
+  for (size_t i = 0; i < NN_ITERATIONS; i++) {
+    gsl_vector_set(input, 0, (double)(rand() % 2));
+    gsl_vector_set(input, 0, (double)(rand() % 2));
+    nn_forward(nn, input);
+    gsl_vector_set(label, 0,
+                   (double)(((int)gsl_vector_get(input, 0)) ^
+                            ((int)gsl_vector_get(input, 1))));
+    nn_backward(nn, label);
+  }
+
+  gsl_vector *errors = gsl_vector_alloc(10);
+  for (size_t i = 0; i < 10; i++) {
+    gsl_vector_set(input, 0, (double)(rand() % 2));
+    gsl_vector_set(input, 0, (double)(rand() % 2));
+    nn_forward(nn, input);
+    gsl_vector_set(label, 0,
+                   (double)(((int)gsl_vector_get(input, 0)) ^
+                            ((int)gsl_vector_get(input, 1))));
+    nn_backward(nn, label);
+    gsl_vector_set(errors, i, vector_avg(nn->e1));
+  }
+
+  ASSERT_LE(vector_avg(errors), NN_MAX_AVG_ERR);
+
+  gsl_vector_free(errors);
+  gsl_vector_free(input);
+  gsl_vector_free(label);
+  nn_free(nn);
+}
 
 #define NN_PATH "nn_test"
 
