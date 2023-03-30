19c01dc900d376df4116485f6ec77992aa3ea2d1
kurdsf <user.kurtpieper@protonmail.com>
2023-03-30 15:16:51 +0200

    Add warning to gan_gen_sample

--- gan.h
+++ gan.h
@@ -15,14 +15,16 @@ typedef struct {
   // shall return false if you just wrote the last sample of your
   // data set onto
   // inputs, else true.
-  bool (*next_sample)(gsl_vector *inputs);
+  bool (*next_sample)(gsl_vector *sample);
   nn_t *gen;
   nn_t *dis;
 } gan_t;
 
-gan_t *new_gan(size_t isize, bool (*next_sample)(gsl_vector *inputs));
+gan_t *new_gan(size_t isize, bool (*next_sample)(gsl_vector *sample);
 // epochs is how often we iterate over the data set.
 void gan_train(size_t epochs);
+// ATTENTION: The vector this returns should not be freed.
+// If you want to free it, you have to free the whole GAN via gan_free.
 gsl_vector *gan_gen_sample(gan_t *gan);
 
 void gan_free(gan_t *gan);
